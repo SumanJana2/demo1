19c3a715e2e77a5d5177125118b7a47ab815df15
SumanJana2 <420dominicontop@gmail.com>
2025-08-26 20:56:37 +0530

    fd

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 
 const Login = ()=>{
-const [formData , setformData]= useState({email:"",password:""});
+const [formData , setformData] = useState({email:"",password:""});
 
 const navigate = useNavigate()
 const handleChange=(e)=>{
